ab4cffcfa67d7c7fa78d2f9fab4ddeb150a2ae02
jhonatan <jhonatan.antunes@vixteam.com.br>
2020-03-05 01:30:12 -0300

    base dos projetos de teste

---
+++ tests/AirplaneProject.ComponentTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AirplaneProject.ComponentTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
