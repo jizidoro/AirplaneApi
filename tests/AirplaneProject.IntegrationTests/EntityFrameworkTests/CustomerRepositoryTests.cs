3b7ec6b5accb5a1e09be49bb06cf8aeb576cebf0
jhonatan <jhonatan.antunes@vixteam.com.br>
2020-03-05 01:57:34 -0300

    base dos testes de integracao

--- tests/AirplaneProject.IntegrationTests/EntityFrameworkTests/CustomerRepositoryTests.cs
+++ tests/AirplaneProject.IntegrationTests/EntityFrameworkTests/CustomerRepositoryTests.cs
@@ -3,60 +3,61 @@ namespace AirplaneProject.IntegrationTests.EntityFrameworkTests
     using System.Linq;
     using System.Threading.Tasks;
     using Aggregates.Internal;
+    using AirplaneProject.Infrastructure.Data;
     using Microsoft.EntityFrameworkCore;
     using Xunit;
 
     public sealed class CustomerRepositoryTests
     {
-        [Fact]
-        public async Task Add_ChangesDatabase()
-        {
-            var options = new DbContextOptionsBuilder<Airpla>()
-                .UseInMemoryDatabase(databaseName: "test_database")
-                .Options;
+        //[Fact]
+        //public async Task Add_ChangesDatabase()
+        //{
+        //    var options = new DbContextOptionsBuilder<AirplaneProjectContext>()
+        //        .UseInMemoryDatabase(databaseName: "test_database")
+        //        .Options;
 
-            var factory = new EntityFactory();
+        //    var factory = new EntityFactory();
 
-            var customer = factory.NewCustomer(
-                new SSN("198608177955"),
-                new Name("Ivan Paulovich"));
+        //    var customer = factory.NewCustomer(
+        //        new SSN("198608177955"),
+        //        new Name("Ivan Paulovich"));
 
-            var user = factory.NewUser(
-                customer.Id,
-                new ExternalUserId("github/ivanpaulovich"));
+        //    var user = factory.NewUser(
+        //        customer.Id,
+        //        new ExternalUserId("github/ivanpaulovich"));
 
-            using (var context = new MangaContext(options))
-            {
-                context.Database.EnsureCreated();
+        //    using (var context = new MangaContext(options))
+        //    {
+        //        context.Database.EnsureCreated();
 
-                var userRepository = new UserRepository(context);
-                await userRepository.Add(user);
+        //        var userRepository = new UserRepository(context);
+        //        await userRepository.Add(user);
 
-                var customerRepository = new CustomerRepository(context);
-                await customerRepository.Add(customer);
+        //        var customerRepository = new CustomerRepository(context);
+        //        await customerRepository.Add(customer);
 
-                Assert.Equal(2, context.Customers.Count());
-            }
-        }
+        //        Assert.Equal(2, context.Customers.Count());
+        //    }
+        //}
 
-        [Fact]
-        public async Task Get_ReturnsCustomer()
-        {
-            var options = new DbContextOptionsBuilder<MangaContext>()
-                .UseInMemoryDatabase(databaseName: "test_database")
-                .Options;
+        //[Fact]
+        //public async Task Get_ReturnsCustomer()
+        //{
+        //    var options = new DbContextOptionsBuilder<MangaContext>()
+        //        .UseInMemoryDatabase(databaseName: "test_database")
+        //        .Options;
 
-            ICustomer customer = null;
+        //    ICustomer customer = null;
 
-            using (var context = new MangaContext(options))
-            {
-                context.Database.EnsureCreated();
+        //    using (var context = new MangaContext(options))
+        //    {
+        //        context.Database.EnsureCreated();
 
-                var repository = new CustomerRepository(context);
-                customer = await repository.GetBy(SeedData.DefaultCustomerId);
+        //        var repository = new CustomerRepository(context);
+        //        customer = await repository.GetBy(SeedData.DefaultCustomerId);
 
-                Assert.NotNull(customer);
-            }
-        }
+        //        Assert.NotNull(customer);
+        //    }
+        //}
     }
 }
